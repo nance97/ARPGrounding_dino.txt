626966ef2c1201284b351e8227c5d2dc71436b7a
nance97 <nancy.kalaj@studenti.unipd.it>
2025-12-07 17:08:41 +0100

    added fusion module

--- inference_arpgrounding.py
+++ inference_arpgrounding.py
@@ -165,8 +165,8 @@ if __name__ == "__main__":
 
             elif args["dinotxt_eval"]:
                 # images are already [B, 3, H, W] from the ARPG dataset
-                heatmaps = dinotxt_backend.get_heatmaps(real_imgs, texts, use_cls=True).detach()
-                neg_heatmaps = dinotxt_backend.get_heatmaps(real_imgs, neg_texts, use_cls=True).detach()
+                heatmaps = dinotxt_backend.get_heatmaps(real_imgs, texts).detach()
+                neg_heatmaps = dinotxt_backend.get_heatmaps(real_imgs, neg_texts).detach()
 
             for j in range(0, len(heatmaps)):
                 pos_regions = [
